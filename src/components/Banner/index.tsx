aadbd33a3e2bd39c14775b6efd1361330817dcc3
yommexg <hydrokidb@gmail.com>
2024-03-07 19:24:52 +0100

    Update

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,18 +1,38 @@
+import { Link } from "react-router-dom";
 import SearchInput from "../../utils/search";
 
-const banner1 = new URL("../../assets/banner/banner1.jpg", import.meta.url)
-  .href;
+const banner1 = new URL("../../assets/banner/banner.jpg", import.meta.url).href;
 
 const Banner = () => {
   return (
     <div
       style={{ backgroundImage: `url(${banner1})` }}
-      className="bg-cover bg-center h-screen lg:h-[32rem] flex items-center justify-center flex-col gap-5"
+      className="bg-cover bg-center h-screen sm:h-[40rem] flex items-center justify-center flex-col gap-10 text-white mt-18"
     >
-      {/* <h1>11G Autos</h1> */}
-      <h2 className="text-white text-xl font-bold">
+      <h1 className="text-5xl font-bold">11G Autos</h1>
+      <h2 className="text-xl font-semibold italic">
         Car Buying to Shape Your Life
       </h2>
+      <div className="gap-7 flex text-sm">
+        <Link
+          className="hover:opacity-60 active:opcity-30 border-b pb-3 hover:border-gray-400 active:border-gray-600"
+          to="/new-cars"
+        >
+          Shop New
+        </Link>
+        <Link
+          className="hover:opacity-60 active:opcity-30  border-b pb-3 hover:border-gray-400 active:border-gray-600"
+          to="/used-cars"
+        >
+          Shop Used
+        </Link>
+        <Link
+          className="hover:opacity-60 active:opcity-30  border-b pb-3 hover:border-gray-400 active:border-gray-600"
+          to="/car-ass"
+        >
+          Shop Assesories
+        </Link>
+      </div>
       <div className="sm:hidden">
         <SearchInput />
       </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,17 +14,27 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-gray-200 px-4 lg:px-32 py-4 shadow-md">
+    <nav className="bg-gray-200 px-4 lg:px-32 py-4 shadow-md h-18 fixed top-0 right-0 left-0">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex gap-5 lg:gap-12 items-center">
           <MobileSidebar isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
           {!isMenuOpen && <Logo />}
           <div className="hidden lg:flex flex-row gap-5 text-sm">
-            <Link to="/">Home</Link>
-            <Link to="/new-cars">Buy New Cars</Link>
-            <Link to="/used-cars">Buy Used Cars</Link>
-            <Link to="/sell-car">Sell Your Car</Link>
-            <Link to="/car-ass">Buy Car Assesories</Link>
+            <Link to="/" className="hover:opacity-60 active:opcity-30">
+              Home
+            </Link>
+            <Link to="/new-cars" className="hover:opacity-60 active:opcity-30">
+              Buy New Cars
+            </Link>
+            <Link to="/used-cars" className="hover:opacity-60 active:opcity-30">
+              Buy Used Cars
+            </Link>
+            <Link to="/sell-car" className="hover:opacity-60 active:opcity-30">
+              Sell Your Car
+            </Link>
+            <Link to="/car-ass" className="hover:opacity-60 active:opcity-30">
+              Buy Car Assesories
+            </Link>
           </div>
         </div>
         <div className="sm:flex flex-row items-center gap-5 ">
