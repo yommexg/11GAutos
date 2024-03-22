9ceb85b4a01ffec1bf2fb19d8041dec713b487de
yommexg <hydrokidb@gmail.com>
2024-03-22 17:00:41 +0100

    update

--- src/components/Navbar/mobileSidebar.tsx
+++ src/components/Navbar/mobileSidebar.tsx
@@ -17,21 +17,30 @@ const MobileSidebar: React.FC<MobileSidebarProps> = ({
 }) => {
   const location = useLocation();
 
+  const openMenu = () => {
+    toggleMenu();
+    document.body.style.overflow = "hidden";
+  };
+  const closeMenu = () => {
+    toggleMenu();
+    document.body.style.overflow = "";
+  };
+
   return (
-    <div className="lg:hidden z-40" onClick={toggleMenu}>
+    <div className="lg:hidden z-40">
       <div className="cursor-pointer">
-        {!isMenuOpen && <FaBars size={25} />}
+        {!isMenuOpen && <FaBars size={25} onClick={openMenu} />}
       </div>
 
       {isMenuOpen && (
         <div className="fixed inset-0 bg-white opacity-90">
-          <div className="absolute cursor-pointer ease-in-out duration-1000 w-[200px] transform-translate-y-0 bg-gray-600 py-5 px-8 top-0 bottom-0 left-0 flex flex-col gap-8">
+          <div className="absolute cursor-pointer ease-in-out duration-1000 w-[250px] md:w-[350px] transform-translate-y-0 bg-gray-600 py-5 px-8 top-0 bottom-0 left-0 flex flex-col gap-8">
             <span>
               <Logo />
             </span>
             <span
               className="text-white text-3xl right-1 -top-2 absolute"
-              onClick={toggleMenu}
+              onClick={closeMenu}
             >
               &times;
             </span>
@@ -83,7 +92,7 @@ const MobileSidebar: React.FC<MobileSidebarProps> = ({
               </Link>
             </div>
           </div>
-          <p className="text-white absolute text-[9px] bottom-0 p-4 w-[200px] bg-black">
+          <p className="text-white absolute text-[9px] md:w-[350px] bottom-0 text-center p-4 w-[250px] bg-black">
             © 2024 11G Autos. All rights reserved.
           </p>
         </div>
