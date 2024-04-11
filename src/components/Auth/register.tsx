72ebf42bf891355fd36d29ffcaf4ebb45f61b110
yommexg <hydrokidb@gmail.com>
2024-04-11 07:08:12 +0100

    update

--- src/components/Auth/register.tsx
+++ src/components/Auth/register.tsx
@@ -19,6 +19,20 @@ const Register: React.FC = () => {
   const [country, setCountry] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  const [passwordError, setPasswordError] = useState("");
+
+  const handlePasswordChange = (value: string) => {
+    setPassword(value);
+    const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s).{8,}$/;
+    if (!passwordRegex.test(value)) {
+      setPasswordError(
+        "Password must be at least 8 characters long and contain at least one number, one special character, and no spaces"
+      );
+    } else {
+      setPasswordError("");
+    }
+  };
+
   const handleRegister = () => {
     console.log(email);
   };
@@ -27,13 +41,13 @@ const Register: React.FC = () => {
     <div className="fixed z-50 right-0 top-0 left-0 bottom-0 outline-none focus:outline-none">
       <div className="border-0 rounded-lg shadow-lg flex w-[100%] h-full bg-slate-500 md:bg-white outline-none focus:outline-none">
         <p
-          className="absolute right-5 top-3 text-5xl text-slate-700 cursor-pointer"
+          className="absolute right-5 top-3 text-3xl text-slate-700 cursor-pointer"
           onClick={() => navigate("/")}
         >
           x
         </p>
         <LoginBanner />
-        <div className="md:w-[50%] w-[100%] lg:pl-32 flex gap-8 flex-col justify-center px-8">
+        <div className="md:w-[50%] w-[100%] max-h-[100vh] overflow-y-auto py-8 flex gap-8 flex-col px-8">
           <p className="md:hidden block">
             <Logo />
           </p>
@@ -43,8 +57,8 @@ const Register: React.FC = () => {
               Please input all fields before clicking the register button
             </p>
           </div>
-          <div className="flex flex-col gap-3 lg:w-[50%]">
-            <label htmlFor="username">Username (Minimum 4 characters):</label>
+          <div className="flex flex-col gap-3 lg:w-[80%]">
+            <label htmlFor="username">Username (Minimum 4 Letters):</label>
             <input
               type="text"
               id="username"
@@ -55,16 +69,23 @@ const Register: React.FC = () => {
               className="bg-slate-200 p-2 text-black font-semibold"
             />
 
-            <label htmlFor="password">Password (Minimum 8 characters):</label>
+            <label htmlFor="password">Password: </label>
             <input
               type="password"
               id="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => handlePasswordChange(e.target.value)}
               minLength={8}
               required
               className="bg-slate-200 p-2 text-black font-semibold"
             />
+
+            {passwordError && (
+              <p className="text-sm text-red-400 italic">
+                Password must be at least 8 characters long and contain at least
+                one number, one special character, and no spaces
+              </p>
+            )}
 
             <label htmlFor="address">Address:</label>
             <input
